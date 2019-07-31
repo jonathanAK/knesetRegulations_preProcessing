76b4f97447cebef8ebf7972e117c07661fad74ca
jonathanak <jonathan@amitkohn.com>
2019-07-31 19:27:18 +0300

    remove .idea

---
+++ index.js
@@ -0,0 +1,35 @@
+const fs = require('fs');
+
+const inputFileName = 'RulesOfProcedure.json';
+const outputFileName = 'output.json';
+const firstContentPage = 31;
+const lastContentPage = 33;
+
+let input = null;
+let data;
+const output = {parts:[]};
+fs.readFile(inputFileName, 'utf8', function (err, data) {
+    if (err) throw err;
+    input = JSON.parse(data);
+    process(input);
+});
+
+function save(){
+    let dataToSave = JSON.stringify(data);
+    fs.writeFileSync(outputFileName, dataToSave);
+}
+
+function process(input){
+    data = input.document.page;
+    data = data.filter((page)=>{
+        return (page['@index'] >= firstContentPage && page['@index']<= lastContentPage);
+    });
+    data.forEach((page)=>{
+        page.row.forEach((row)=>{
+
+        });
+    });
+    save();
+}
+
+
